8186b86d1dc405ec2025da78b5ce0eb6564c3dec
btk15049 <btk15049@live.jp>
2023-12-06 16:21:05 -0800

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -22,48 +22,68 @@ vector<vector<int>> g;
 int64_t d_sum = 0;
 
 struct Score {
-    // 未訪問のマスに対する係数
-    int64_t xc = 0;
-    // 1 回訪問したマスに対する係数
-    int64_t yc = 0;
-
-    inline int64_t f(int l) const { return l * (l + int64_t(1)); }
-
-    int64_t z = 0;
-    inline void add_z(int l, int64_t d) { z += d * f(l); }
-    inline void sub_z(int l, int64_t d) { z -= d * f(l); }
-
-    inline double calc_length_penalty(int l) const {
-        const double length_threshold = N * N * LENGTH_THRESHOLD;
-        return l > length_threshold ? (l - length_threshold) / length_threshold
-                                    : 0.0;
+    int64_t never_visited    = 0;
+    int64_t once_visited     = 0;
+    int64_t multiple_visited = 0;
+    void add_multiple_visited(int64_t subseq_len, int64_t d) {
+        multiple_visited += d * subseq_len * (subseq_len + 1);
     }
-
-    inline double val(int l, double c_x, double c_l) const {
-        if (l < N * N) l = N * N;
-        const double length_penalty = calc_length_penalty(l);
-        return (c_x * xc * f(l) + yc * f(l - 1) + z) / (2.0 * l)
-               + c_l * length_penalty;
+    int64_t len = 0;
+    inline double calc_length_penalty() const {
+        const double length_threshold = N * N * LENGTH_THRESHOLD;
+        return len > length_threshold
+                   ? (len - length_threshold) / length_threshold
+                   : 0.0;
+    }
+    inline double score_with_penalty(double never_visited_penalty,
+                                     double length_penalty_coef) const {
+        auto l                      = max(len, int64_t(N) * N);
+        const double length_penalty = calc_length_penalty();
+        return (never_visited_penalty * never_visited * l * (l + 1) // 未訪問
+                + once_visited * l * (l - 1) // 1 回訪問
+                + multiple_visited           // 複数回訪問
+                ) / (2.0 * len)
+               + length_penalty_coef * length_penalty;
     }
 };
+
+// 差分計算用構造体
+struct F {
+    int64_t a = 0;
+    int64_t b = 0;
+    int64_t c = 0;
+};
+
+// 差分計算用構造体
+struct ScoreDiffManager {
+    // 未訪問のマスにおける汚れの合計
+    int64_t xc = 0;
+    // 1 回訪問したマスにおける汚れの合計
+    int64_t yc = 0;
+
+    // 複数回現れる項の管理
+    vector<F> fs;
+};
+
+
+struct State;
 
 struct State {
     vector<int16_t> seq;
     Score score;
-
     vector<int> visited_count;
     vector<pair<int, int>> visited_first_last;
     State() {
         seq.clear();
-        score    = Score();
-        score.xc = d_sum;
         visited_count.resize(N * N);
         visited_first_last.resize(N * N);
     }
 
+
     void sync_score() {
-        score    = Score();
-        score.xc = d_sum;
+        score               = Score();
+        score.len           = seq.size() - 1;
+        score.never_visited = d_sum;
         fill(visited_count.begin(), visited_count.end(), 0);
         fill(visited_first_last.begin(), visited_first_last.end(),
              make_pair(-1, -1));
@@ -77,18 +97,18 @@ struct State {
             auto& last = visited_first_last[v].second;
             switch (visited_count[v]) {
                 case 0:
-                    score.xc -= D[v];
-                    score.yc += D[v];
+                    score.never_visited -= D[v];
+                    score.once_visited += D[v];
                     break;
                 case 1:
-                    score.yc -= D[v];
+                    score.once_visited -= D[v];
                     assert(last != -1);
-                    score.add_z(i - last - 1, D[v]);
+                    score.add_multiple_visited(i - last - 1, D[v]);
                     multiple_visited.push_back(v);
                     break;
                 default:
                     assert(last != -1);
-                    score.add_z(i - last - 1, D[v]);
+                    score.add_multiple_visited(i - last - 1, D[v]);
                     break;
             }
             visited_count[v]++;
@@ -102,7 +122,8 @@ struct State {
 
         for (int v : multiple_visited) {
             const auto [first, last] = visited_first_last[v];
-            score.add_z((seq.size() - last - 1) + (first - 1), D[v]);
+            score.add_multiple_visited((seq.size() - last - 1) + (first - 1),
+                                       D[v]);
         }
     }
 
@@ -122,6 +143,24 @@ struct State {
         score              = s.score;
         visited_count      = s.visited_count;
         visited_first_last = s.visited_first_last;
+    }
+};
+
+struct ModifiedState {
+    State* state;
+    int bg;
+    int ed;
+    vector<int16_t> new_subseq;
+    Score score;
+
+    State build_state() {
+        State ret = state->copy();
+        ret.seq   = state->seq;
+        ret.seq.erase(ret.seq.begin() + bg, ret.seq.begin() + ed + 1);
+        ret.seq.insert(ret.seq.begin() + bg, new_subseq.begin(),
+                       new_subseq.end());
+        ret.sync_score();
+        return ret;
     }
 };
 
@@ -164,106 +203,6 @@ void init() {
     for_find_path::init();
 }
 
-// 代表頂点を返す
-vector<int> find_cluster() {
-    int threshold = 100;
-
-    static int used[N_UB * N_UB];
-    fill(used, used + N * N, 0);
-    used[0] = 1;
-    vector<int> ret;
-
-    while (true) {
-        // 1. unused なマスのうち d が最大のマスを探す、 d が 100 以下なら終了
-        int max_d = -1;
-        int max_v = -1;
-        for (int i = 0; i < N * N; i++) {
-            if (used[i]) continue;
-            if (D[i] > max_d) {
-                max_d = D[i];
-                max_v = i;
-            }
-        }
-        if (ret.empty() && threshold > max_d) {
-            threshold = max_d / 2;
-        }
-        if (max_d < threshold) break;
-        if (max_v == -1) break;
-        ret.push_back(max_v);
-        // cerr << cell[max_v].to_string() << ": " << max_d << endl;
-
-        // 2. そのマスから隣接したマスのうち
-        // 汚れ d' が log(d') >= log(d) - 1
-        // を満たすもの高いものから順にクラスタに加えていく クラスタのサイズが N
-        // になったら終了
-        set<int> cluster;
-        cluster.insert(max_v);
-        priority_queue<pair<int, int>> pq;
-        pq.emplace(max_d, max_v);
-        used[max_v] = 1;
-        while ((int)cluster.size() < N && !pq.empty()) {
-            const auto [d, v] = pq.top();
-            pq.pop();
-            for (int u : g[v]) {
-                if (used[u]) continue;
-                if (log10(D[u]) >= log10(max_d) - 1) {
-                    cluster.insert(u);
-                    pq.emplace(D[u], u);
-                    used[u] = 1;
-                }
-            }
-        }
-        while (!pq.empty()) {
-            // used を戻す
-            const auto v = pq.top().second;
-            pq.pop();
-            used[v] = 0;
-        }
-    }
-    return ret;
-}
-
-State build_first_state() {
-    State best;
-    double best_score = 1e18;
-
-
-    auto cluster = find_cluster();
-
-    // 何回か回して最良のものを返す
-    scheduler::Scheduler<300> timer;
-
-    while (timer.update()) {
-        State s;
-        xorshift::shuffle(cluster);
-        s.add_back(0);
-        for (int k = 0; k < 1; k++) {
-            for (int v : cluster) {
-                auto path = find_path(s.seq.back(), v);
-                assert(path.front() == s.seq.back());
-                assert(path.back() == v);
-                for (size_t p_i = 1; p_i < path.size(); p_i++) {
-                    s.add_back(path[p_i]);
-                }
-            }
-        }
-        {
-            auto path = find_path(s.seq.back(), 0);
-            assert(path.front() == s.seq.back());
-            assert(path.back() == 0);
-            for (size_t p_i = 1; p_i < path.size(); p_i++) {
-                s.add_back(path[p_i]);
-            }
-        }
-        s.sync_score();
-        const double score = s.score.val(s.seq.size() - 1, 0.0, 0);
-        if (score < best_score) {
-            best_score = score;
-            best       = s;
-        }
-    }
-    return best;
-}
 
 inline int manhattan(int v, int u) {
     auto vc = cell[v];
@@ -449,8 +388,8 @@ State improve(State s) {
 
         double c_x                 = 1 + (1000 - 1) * progress;
         const double c_l           = LENGTH_PENALTY_COEFFICIENT;
-        const double current_score = s.score.val(s.seq.size() - 1, c_x, c_l);
-        const double next_score    = t.score.val(t.seq.size() - 1, c_x, c_l);
+        const double current_score = s.score.score_with_penalty(c_x, c_l);
+        const double next_score    = t.score.score_with_penalty(c_x, c_l);
         const double temperture =
             calc_temperature(timer.progress, timer.limit, T_START, T_END);
         const double prob =
@@ -536,13 +475,14 @@ void dfs(State& state, int v, bool first = true) {
 
 State sample() {
     State best;
-    best.score.z = 1e18;
+    best.score.multiple_visited = 1e18;
+    best.score.len              = 2;
     for (int i = 0; i < 1; i++) {
         State state;
         dfs(state, 0);
         state.sync_score();
-        if (state.score.val(state.seq.size() - 1, 0, 0)
-            < best.score.val(state.seq.size() - 1, 0, 0)) {
+        if (state.score.score_with_penalty(0, 0)
+            < best.score.score_with_penalty(0, 0)) {
             best.copy_from(state);
             // cerr << "update: " << best.score.val(state.seq.size() - 1, 0, 0)
             //      << endl;
@@ -568,10 +508,10 @@ int main() {
     output(ret.seq, cell);
     logger::push("L", int64_t(ret.seq.size() - 1));
 
-    logger::push("score", ret.score.val(ret.seq.size() - 1, 1e10, 0));
+    logger::push("score", ret.score.score_with_penalty(1e10, 0));
     transiton_arm::add_log();
     logger::flush();
-    if (ret.score.xc > 0) {
+    if (ret.score.never_visited > 0) {
         exit(-1);
     }
     return 0;
